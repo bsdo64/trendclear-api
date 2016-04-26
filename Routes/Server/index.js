52f8d71483136db83e9aeaf0223c4fc4df99804b
ByeongSu Do <bsdo@naver.com>
2016-04-26 17:11:27 +0900

    fix merge

--- Routes/Server/index.js
+++ Routes/Server/index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const router = express.Router();
-const Models = require('../../Models');
 
 const M = require('../../Models/index');
 
@@ -13,22 +12,12 @@ router.use(function timeLog(req, res, next) {
 });
 // define the home page route
 router.get('/', function(req, res) {
-<<<<<<< HEAD
-  Models
-    .User
-    .signin()
-    .then(function() {
-      res.send('Hello world')
-    });
-=======
-
   M
     .User
     .signin()
     .then(function (user) {
       res.send(user);
     })
->>>>>>> 4a0c7504d9eecf48429e2cf8d3a2645fe77cd13e
 });
 // define the about route
 router.get('/about', function(req, res) {
